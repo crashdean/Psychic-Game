d550fff39124d89280c3679af7723ff2389ea646
raerae1205 <crashdean@hotmail.com>
2019-04-12 21:19:08 -0700

    update game.js

--- assets/javascripts/game.js
+++ assets/javascripts/game.js
@@ -1,11 +1,52 @@
 //variable for tracking
-var Wins = 0;
-var Losses = 0;
+var wins = 0;
+var losses = 0;
 var guessesLeft = 9;
-var guessesSoFar = [];
+var guessLetters = [];
 var computerGuess = [];
 //Array of possible letters to use
-var keyBoardChoises = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+var keyBoardChoises = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var userGuess = "";
 
-var computerGuess = keyBoardChoises[Math.floor(Math.random() * keyBoardChoises.length)];
-console.log(keyBoardChoises[0]);
+
+function selectHiddenLetter() {
+
+    computerGuess = keyBoardChoises[Math.floor(Math.random() * keyBoardChoises.length)];
+    console.log(computerGuess)
+}
+
+function playGame() {
+    setupGame()
+    document.addEventListener("keyup", function (event) {
+        userGuess = event.key;
+        if (guessLetters.includes(userGuess)) {
+
+        } else {
+            guessLetters.push(userGuess);
+            document.getElementById("unanswered").textContent = guessLetters;
+            if (userGuess === computerGuess) {
+                wins++;
+                document.getElementById("correct").textContent = wins;
+                setupGame()
+            } else {
+                guessesLeft--
+               document.getElementById("guesses-left").textContent=guessesLeft;
+            }
+            if (guessesLeft===0) {
+                losses++
+                document.getElementById("wrong").textContent = losses;
+                setupGame()
+            }
+        }
+
+    });
+
+}
+
+function setupGame() {
+    document.getElementById("unanswered").textContent = "";
+    document.getElementById("guesses-left").textContent="";
+    selectHiddenLetter()
+}
+
+playGame()
